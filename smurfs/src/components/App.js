fe38f8e832e49a95cff9de836a2c4a2a58bdf08e
kmcknight1 <katiemcknight1991@gmail.com>
2019-06-21 12:22:25 -0500

    reactstrap working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from "react";
+
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Smurfs from "./Smurfs";
 import AddSmurf from "./AddSmurf";
+import Landing from "./Landing";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header>
-            <h1>Welcome to Smurfs Village via REDUX!</h1>
-          </header>
-          <div>
-            <Link to="/smurfs">Get those Smurfs!</Link>
-          </div>
+          <Route exact path="/" component={Landing} />
           <Route exact path="/smurfs" component={Smurfs} />
           <Route exact path="/addsmurf" component={AddSmurf} />
         </div>
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./components/App";
 import thunk from "redux-thunk";
@@ -7,6 +8,8 @@ import logger from "redux-logger";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
